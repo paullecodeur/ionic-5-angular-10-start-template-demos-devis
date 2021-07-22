e4ea1404acf8d96951a3102282be0d208510d702
paullecodeur <paulyemdji@gmail.com>
2021-07-22 17:47:51 +0100

    initialisation

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonReorderGroup } from '@ionic/angular';
+import { ItemReorderEventDetail } from '@ionic/core';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,24 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  items = [1, 2, 3, 4, 5];
+
+  @ViewChild(IonReorderGroup) reorderGroup: IonReorderGroup;
+
   constructor() {}
+
+  doReorder(ev: CustomEvent<ItemReorderEventDetail>) {
+    // Before complete is called with the items they will remain in the
+    // order before the drag
+    console.log('Before complete', this.items);
+
+    // Finish the reorder and position the item in the DOM based on
+    // where the gesture ended. Update the items variable to the
+    // new order of items
+    this.items = ev.detail.complete(this.items);
+
+    // After complete is called the items will be in the new order
+    console.log('After complete', this.items);
+  }
 
 }
